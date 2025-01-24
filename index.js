a1392e3698eac2651d6d5ac2f4a944ec717011cc
tusherbhakta <tusheranta@gmail.com>
2025-01-25 01:36:36 +0530

    jwt not working. trying to fixing the bug

--- index.js
+++ index.js
@@ -70,8 +70,8 @@ async function run() {
         // jwt
 
         app.post('/jwt', (req, res) => {
-            const { user } = req.body;
-            const token = jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: '5h' });
+            const { users } = req.body;
+            const token = jwt.sign({ users }, process.env.JWT_SECRET, { expiresIn: '5h' });
             res.cookie('token', token, cookieOptions).send({ success: 'Token sent' });
         });
 
